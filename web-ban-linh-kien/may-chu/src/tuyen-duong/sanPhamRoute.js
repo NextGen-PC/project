6df814ffa8566ec79edb5d154229dd458d3a4025
tuandevdt <doanthanhtuan1998@gmail.com>
2026-03-01 21:07:36 +0700

    init source

--- web-ban-linh-kien/may-chu/src/tuyen-duong/sanPhamRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { layDanhSachSanPham, taoDuLieuMau } = require('../bo-dieu-khien/sanPhamController.js');
-
-router.get('/', layDanhSachSanPham); // Gọi: GET /api/san-pham
-router.post('/tao-mau', taoDuLieuMau); // Gọi: POST /api/san-pham/tao-mau
-
-module.exports = router;
